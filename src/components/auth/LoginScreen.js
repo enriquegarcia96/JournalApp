bd9fe18e5ee775c7cb0c291a53552da0245a539e
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-17 13:05:07 -0600

    refactor: 💡 estilos al registro screnn

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -30,7 +30,7 @@ export const LoginScreen = () => {
           </div>
         </div>
 
-        <Link to="/auth/register"  >
+        <Link to="/auth/register" className="link" >
 
           Create new account
         </Link>
