e386080a7567a7f977c3a35d550debf515f0bfc3
enriquegarcia96 <enriquegarcia259@yahoo.es>
2022-03-18 09:37:47 -0600

    style: 💄 estructura y diseño del homeScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import { NotesAppBar } from './NotesAppBar'
+import React from "react";
+import { NotesAppBar } from "./NotesAppBar";
 
 export const NoteScreen = () => {
   return (
-    <div className='notes__main-content' >
-        <NotesAppBar />
+    <div className="notes__main-content">
+      <NotesAppBar />
 
+      <div className="note__content">
+        <input
+          type="text"
+          placeholder="Some awesome title"
+          className="notes__title-input"
+          autoComplete="off"
+        />
+        <textarea
+          placeholder="What happend today"
+          className="notes__textare"
+        ></textarea>
 
+        <div className="notes__image">
+          <img
+            src="https://d5nunyagcicgy.cloudfront.net/external_assets/hero_examples/hair_beach_v391182663/original.jpeg"
+            alt="imagen"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
